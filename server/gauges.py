db4ad8c90287ec6588cfe44d8aaaeb1f80f104f6
Richard Barnes <rbarnes@umn.edu>
2015-04-12 15:35:16 -0500

    Get flow info

--- server/gauges.py
+++ server/gauges.py
@@ -18,14 +18,22 @@ def show_gaugelist(xmin,ymin,xmax,ymax):
   cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 
   cur.execute("""
-    SELECT  ST_X(geom) as lng, ST_Y(geom) as lat
-    FROM   gageloc
-    WHERE  geom
+SELECT *, (SELECT percent_rank(value) WITHIN GROUP (ORDER BY ave ASC) as rank
+              FROM gage_smooth
+              WHERE month=13 and year>=1985 and site_no=site_code
+              GROUP BY site_no
+          )
+FROM (SELECT source_fea AS site_code, ST_X(geom) as lng, ST_Y(geom) as lat
+        FROM   gageloc
+        WHERE  geom
         @ -- contained by, gets fewer rows -- ONE YOU NEED!
         ST_MakeEnvelope (
-            %(xmin)s, %(ymin)s, -- bounding
-            %(xmax)s, %(ymax)s, -- box limits
-            900913) order by random() LIMIT 500
+          %(xmin)s, %(ymin)s, -- bounding
+          %(xmax)s, %(ymax)s, -- box limits
+          900913
+        )
+        ORDER BY random() LIMIT 500) AS c
+NATURAL JOIN (SELECT a.* FROM gauge_data AS a JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b ON a.site_code = b.site_code AND a.variable=b.variable AND a.dt = b.maxDate) AS d
   """, {"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax})
 
   return json.dumps(cur.fetchall())
