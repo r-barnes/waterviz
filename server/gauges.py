319c591702f63cecbdc0a3268c5f2f1a0b547ca9
Richard Barnes <rbarnes@umn.edu>
2015-04-12 17:22:46 -0500

    Switch order of variables

--- server/gauges.py
+++ server/gauges.py
@@ -21,12 +21,12 @@ def show_reachflow(huc8):
 
   cur.execute("""
 SELECT avg(dvalue) as dvalue, avg(svalue) as svalue, avg(drank) as drank FROM (
-    SELECT a.site_code,a.dt as ddt, a.value as dvalue FROM gauge_data AS a
+    SELECT a.site_code,a.dt as sdt, a.value as svalue FROM gauge_data AS a
     JOIN (
       SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b
-    ON a.site_code = b.site_code AND a.variable='D' AND a.variable=b.variable AND a.dt = b.maxDate) as d
+    ON a.site_code = b.site_code AND a.variable='S' AND a.variable=b.variable AND a.dt = b.maxDate) as d
     NATURAL JOIN (
-      SELECT a.site_code,a.dt as sdt, a.value as svalue,
+      SELECT a.site_code,a.dt as ddt, a.value as dvalue,
       (SELECT percent_rank(a.value) WITHIN GROUP (ORDER BY ave ASC) as drank
          FROM gage_smooth
          WHERE month=13 and year>=1985 and site_no=a.site_code
@@ -34,7 +34,7 @@ SELECT avg(dvalue) as dvalue, avg(svalue) as svalue, avg(drank) as drank FROM (
       ) as drank
       FROM gauge_data AS a
       JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b
-      ON a.site_code = b.site_code AND a.variable='S' AND a.variable=b.variable AND a.dt = b.maxDate) AS e
+      ON a.site_code = b.site_code AND a.variable='D' AND a.variable=b.variable AND a.dt = b.maxDate) AS e
 WHERE site_code IN (SELECT source_fea FROM gageloc WHERE reachcode LIKE %(huc8)s)
 """, {"huc8":huc8})
 
