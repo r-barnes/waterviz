eb49643b9dbc5eae0c784a2a8a1d73afdd83a019
Richard Barnes <rbarnes@umn.edu>
2015-04-12 17:17:16 -0500

    Provide historical context

--- server/gauges.py
+++ server/gauges.py
@@ -15,11 +15,24 @@ conn = psycopg2.connect("dbname='rivers' user='nelson' host='localhost' password
 
 @app.route('/gauges/reachflow/<string:huc8>', methods=['GET'])
 def show_reachflow(huc8):
-  cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+  cur = conn.cursor() #cursor_factory = psycopg2.extras.RealDictCursor)
 
   cur.execute("""
-SELECT avg(dvalue) as dvalue, avg(svalue) as svalue FROM (SELECT a.site_code,a.dt as ddt, a.value as dvalue FROM gauge_data AS a JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b ON a.site_code = b.site_code AND a.variable='D' AND a.variable=b.variable AND a.dt = b.maxDate) as d
-NATURAL JOIN (SELECT a.site_code,a.dt as sdt, a.value as svalue FROM gauge_data AS a JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b ON a.site_code = b.site_code AND a.variable='S' AND a.variable=b.variable AND a.dt = b.maxDate) AS e
+SELECT avg(dvalue) as dvalue, avg(svalue) as svalue FROM (
+    SELECT a.site_code,a.dt as ddt, a.value as dvalue FROM gauge_data AS a
+    JOIN (
+      SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b
+    ON a.site_code = b.site_code AND a.variable='D' AND a.variable=b.variable AND a.dt = b.maxDate) as d
+    NATURAL JOIN (
+      SELECT a.site_code,a.dt as sdt, a.value as svalue,
+      (SELECT percent_rank(dvalue) WITHIN GROUP (ORDER BY ave ASC) as drank
+         FROM gage_smooth
+         WHERE month=13 and year>=1985 and site_no=site_code
+         GROUP BY site_no
+      ) as drank
+      FROM gauge_data AS a
+      JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b
+      ON a.site_code = b.site_code AND a.variable='S' AND a.variable=b.variable AND a.dt = b.maxDate) AS e
 WHERE site_code IN (SELECT source_fea FROM gageloc WHERE reachcode LIKE '%s%%')
 """, huc8)
 
