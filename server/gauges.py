ccbf2a9e6ba5f099a9fa08d5f2e569b37ea40099
Richard Barnes <rbarnes@umn.edu>
2015-04-12 18:16:59 -0400

    Info works

--- server/gauges.py
+++ server/gauges.py
@@ -17,11 +17,13 @@ conn = psycopg2.connect("dbname='rivers' user='nelson' host='localhost' password
 def show_reachflow(huc8):
   cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 
+  huc8+='%'
+
   cur.execute("""
 SELECT avg(dvalue) as dvalue, avg(svalue) as svalue FROM (SELECT a.site_code,a.dt as ddt, a.value as dvalue FROM gauge_data AS a JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b ON a.site_code = b.site_code AND a.variable='D' AND a.variable=b.variable AND a.dt = b.maxDate) as d
 NATURAL JOIN (SELECT a.site_code,a.dt as sdt, a.value as svalue FROM gauge_data AS a JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b ON a.site_code = b.site_code AND a.variable='S' AND a.variable=b.variable AND a.dt = b.maxDate) AS e
-WHERE site_code IN (SELECT source_fea FROM gageloc WHERE reachcode LIKE '%s%%')
-""", huc8)
+WHERE site_code IN (SELECT source_fea FROM gageloc WHERE reachcode LIKE %(huc8)s)
+""", {"huc8":huc8})
 
   return json.dumps(cur.fetchall()[0])
 
