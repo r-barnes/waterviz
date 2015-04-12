580e9f92f00df6ecc71b98fca27713265d3b23ce
Richard Barnes <rbarnes@umn.edu>
2015-04-12 03:26:57 -0500

    Don't eliminate old files on restart

--- server/serve.sh
+++ server/serve.sh
@@ -3,7 +3,7 @@
 # Launch a Gunicorn server to serve vector tiles
 
 # Remove old cached files; not necessary, may be harmful in production
-rm -rf /tmp/stache
+#rm -rf /tmp/stache
 
 # Launch gunicorn
 gunicorn --error-logfile=- -c gunicorn.cfg.py 'TileStache:WSGITileServer("tilestache.cfg")'
