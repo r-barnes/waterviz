665110f1c0e9e41d82604b9c2a49353f779097a8
Richard Barnes <rbarnes@umn.edu>
2015-04-12 15:54:08 -0500

    Return more data!

--- server/gauges.py
+++ server/gauges.py
@@ -19,7 +19,7 @@ def show_gaugelist(xmin,ymin,xmax,ymax):
 
 
   cur.execute("""
-SELECT site_code, lng, lat, variable, to_char(dt, 'YYYY-MM-DD HH24:MI') as dt, value, (SELECT percent_rank(value) WITHIN GROUP (ORDER BY ave ASC) as rank
+SELECT site_code, lng, lat, to_char(sdt, 'YYYY-MM-DD HH24:MI') as sdt, svalue, to_char(sdt, 'YYYY-MM-DD HH24:MI') as ddt, dvalue, (SELECT percent_rank(dvalue) WITHIN GROUP (ORDER BY ave ASC) as drank
               FROM gage_smooth
               WHERE month=13 and year>=1985 and site_no=site_code
               GROUP BY site_no
@@ -34,7 +34,8 @@ FROM (SELECT source_fea AS site_code, ST_X(geom) as lng, ST_Y(geom) as lat
           900913
         )
         ORDER BY random() LIMIT 500) AS c
-NATURAL JOIN (SELECT a.* FROM gauge_data AS a JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b ON a.site_code = b.site_code AND a.variable=b.variable AND a.dt = b.maxDate) AS d
+NATURAL JOIN (SELECT a.site_code,a.dt as ddt, a.value as dvalue FROM gauge_data AS a JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b ON a.site_code = b.site_code AND a.variable='D' AND a.variable=b.variable AND a.dt = b.maxDate) AS d
+NATURAL JOIN (SELECT a.site_code,a.dt as sdt, a.value as svalue FROM gauge_data AS a JOIN (SELECT site_code, variable, max(dt) maxDate FROM gauge_data GROUP BY site_code,variable) b ON a.site_code = b.site_code AND a.variable='S' AND a.variable=b.variable AND a.dt = b.maxDate) AS e
   """, {"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax})
 
   return json.dumps(cur.fetchall())
