921134b1800d075cefb0178ca657223dd08a411f
Richard Barnes <rbarnes@umn.edu>
2015-04-12 22:16:37 -0500

    Adjust river size

--- clients/lib/client.js
+++ clients/lib/client.js
@@ -65,7 +65,7 @@ function StyleTheRivers(feature){
   requestsPool.fetch('/gauges/reachflow/'+feature.properties.huc8)
   .done(function(val){
     $('.h'+feature.properties.huc8).css('stroke', (val.drank!==null)?grad_colours[Math.floor((grad_colours.length-1)*val.drank)]:'#FF00DE' );
-    $('.h'+feature.properties.huc8).css('stroke-width', ((val.drank!==null)?(6*val.drank).toString():'2')+'px' );
+    $('.h'+feature.properties.huc8).css('stroke-width', ((val.drank!==null)?(6*val.drank+2).toString():'2')+'px' );
   });
 }
 //$.getJSON('/gauges/reachflow/'+feature.properties.huc8, function(data){
