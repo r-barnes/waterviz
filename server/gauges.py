41549f1f11edabd6e2a1bf4a2393bfef630c7cf7
Richard Barnes <rbarnes@umn.edu>
2015-04-22 22:20:44 -0500

    Need a cursor

--- server/gauges.py
+++ server/gauges.py
@@ -93,6 +93,7 @@ NATURAL JOIN (SELECT a.site_code,a.dt as sdt, a.value as svalue FROM gauge_data 
 
 @app.route('/hurricanes/<string:date>')
 def show_hurricanes(date):
+  cur = g.db.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
   cur.execute("""SELECT * from hurricane where stormid in (select distinct stormid from hurricane where iso_time=%(time)s)""", {"time:":date})
   return json.dumps(cur.fetchall())
 
