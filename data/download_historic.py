2e8d4aa23924a09572d5522f1b588fb7455db093
Richard Barnes <rbarnes@umn.edu>
2015-04-25 20:49:42 -0500

    Added site info note

--- data/download_historic.py
+++ data/download_historic.py
@@ -13,6 +13,7 @@ cur  = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 
 cur.execute("SELECT DISTINCT source_fea as site_no FROM gageloc")
 
+#See, e.g., http://waterdata.usgs.gov/nwis/dv/?site_no=09306620 for site info
 for site in cur.fetchall():
   url = "http://waterdata.usgs.gov/nwis/dv?cb_00065=on&cb_00060=on&format=rdb&site_no=SITE_NUMBER&referred_module=sw&period=&begin_date=2007-10-01&end_date=2015-04-24"
   url = url.replace('SITE_NUMBER',site['site_no'])
