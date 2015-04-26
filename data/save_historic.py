9a87661bfae7b38122fdf1adb86e5ff840e31ddb
Richard Barnes <rbarnes@umn.edu>
2015-04-26 13:04:42 -0500

    Set None when we cannot know drank

--- data/save_historic.py
+++ data/save_historic.py
@@ -43,7 +43,8 @@ for i,fname in enumerate(glob.glob('gages_historic/*_historic.dat')):
       i['dvalue'] = i[key60[0]]
       if not i['dvalue'] or not i['dvalue'].isdigit(): #Filter empty strings
         i['dvalue'] = None
-      if i['site_no'] in historic_data:
+        i['drank']  = None
+      elif i['site_no'] in historic_data:
         i['drank'] = scipy.stats.percentileofscore(historic_data[i['site_no']],i['dvalue'])
       else:
         i['drank'] = None
