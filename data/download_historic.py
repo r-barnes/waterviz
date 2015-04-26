1bddfeaa30e32a6b64d8644cda3c1df9f5bfcd2f
Richard Barnes <rbarnes@umn.edu>
2015-04-25 20:44:15 -0500

    Use appropriate columns

--- data/download_historic.py
+++ data/download_historic.py
@@ -11,7 +11,7 @@ if not os.path.exists('gages_historic'):
 conn = psycopg2.connect("dbname='rivers' user='nelson' host='localhost' password='NONE'")
 cur  = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 
-cur.execute("SELECT DISTINCT source_fea FROM gageloc")
+cur.execute("SELECT DISTINCT source_fea as site_no FROM gageloc")
 
 for site in cur.fetchall():
   url = "http://waterdata.usgs.gov/nwis/dv?cb_00065=on&cb_00060=on&format=rdb&site_no=SITE_NUMBER&referred_module=sw&period=&begin_date=2007-10-01&end_date=2015-04-24"
